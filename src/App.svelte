4a9d12a864de04015db6d5a708d91d4f0654cef8
thijsheijden <hi@thijsheijden.nl>
2022-01-05 16:35:59 +0100

    Added basic timeline

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
   import Splashscreen from './sections/Splashscreen.svelte'
   import Money from './sections/Money.svelte'
   import Contact from './sections/Contact.svelte'
+  import Timeline from './sections/Timeline.svelte'
 </script>
 
 <Splashscreen/>
 <WhatWeDo/>
+<Timeline/>
 <Money/>
 <Contact/>
