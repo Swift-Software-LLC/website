dd2e08cbd5474d10a50ac24b498c57218e9a9867
thijsheijden <hi@thijsheijden.nl>
2022-01-08 15:33:55 +0100

    Updated costs section

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,14 @@
   import WhatWeDo from './sections/What-we-do.svelte'
   import Splashscreen from './sections/Splashscreen.svelte'
   import WhyAMVP from './sections/Why-a-mvp.svelte'
-  import Money from './sections/Money.svelte'
+  import Cost from './sections/Cost.svelte'
   import Contact from './sections/Contact.svelte'
   import Timeline from './sections/Timeline.svelte'
-import WhyAMvp from './sections/Why-a-mvp.svelte';
 </script>
 
 <Splashscreen/>
-<WhyAMvp/>
+<WhyAMVP/>
 <WhatWeDo/>
 <Timeline/>
+<Cost/>
 <Contact/>
---
+++ src/sections/Cost.svelte
@@ -0,0 +1,44 @@
+<section style="width: 100%; background-color: #000; color: #fff;">
+  <section id="cost">
+    <h1 class="header">Sounds great! But, how much does it cost?</h1>
+    <p>
+      It's hard to give a concrete number, as the cost for a MVP depends on many different factors.
+      Larger, more complex projects will logically cost more than simple one page applications.
+    </p>
+    <p>
+      As an estimate, think of around $20,000 per month of development time, for a medium complex MVP.
+      To get a more accurate price estimate, <a href="" onclick={"Calendly.initPopupWidget({url: 'https://calendly.com/swiftsoftware/introduction-call'});return false;"}>schedule a call</a>.
+    </p>
+    <p>
+      As we know, many startups, especially those in the pre-seed phase, don't have this kind of capital.
+      This is why we offer <span class="gradient-text">part payment in the form of equity</span>. Think of it as a non-monetary investment into your startup.
+    </p>
+  </section>
+</section>
+
+<style>
+  /* Phone styling */
+  section {
+    padding-top: 1rem;
+    margin: 0 auto 0 auto;
+    padding: 2rem 0 2rem 0;
+  }
+
+  #cost {
+    width: 90%;
+    margin: 0 auto 0 auto;
+  }
+
+  p {
+    font-size: 1.25rem;
+    margin: 1rem 0 1rem 0;
+  }
+
+  /* Desktop styling */
+  @media screen and (min-width: 1200px) {
+    #cost {
+      width: 40%;
+    }
+  }
+
+</style>
